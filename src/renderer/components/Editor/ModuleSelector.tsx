850411f9636b8cf877c2dea9023867a7832dd08e
zhangxuyang <240283142@qq.com>
2021-07-06 01:55:22 +0800

    feat: module selector 效果

--- src/renderer/components/Editor/ModuleSelector.tsx
+++ src/renderer/components/Editor/ModuleSelector.tsx
@@ -5,14 +5,14 @@ import logSymbols from "log-symbols";
 import { useModuleList, useModuleConf } from "@/hooks/source";
 
 import { Tooltip } from "antd";
-import { useGetSourceImageUrl } from "@/hooks/image";
-// import SourceImage from "../Image/SourceImage";
+// import { useGetSourceImageUrl } from "@/hooks/image";
+import SourceImage from "../Image/SourceImage";
 
 // 模块选择器
 const ModuleSelector: React.FC = () => {
   const moduleList = useModuleList();
   const [currentModule, setCurrentModule] = useModuleConf();
-  const getImageURL = useGetSourceImageUrl();
+  // const getImageURL = useGetSourceImageUrl();
 
   if (!currentModule) {
     console.log(logSymbols.error, "currentModule 为空");
@@ -27,16 +27,21 @@ const ModuleSelector: React.FC = () => {
   return (
     <StyleModuleSelector>
       {moduleList.map((item, key) => (
-        <StyleIcon
-          key={key}
-          isActive={currentModule.index === item.index}
-          onClick={() => setCurrentModule(item)}
-        >
-          <Tooltip title={item.name} placement="right">
-            <img className="icon" alt="" src={getImageURL(item.icon)} />
-            {/* <SourceImage className="icon" src={item.icon} /> */}
-          </Tooltip>
-        </StyleIcon>
+        <Tooltip key={key} title={item.name} placement="right">
+          <StyleIcon onClick={() => setCurrentModule(item)}>
+            {/* <img
+              className="icon"
+              data-isActive={String(currentModule.index === item.index)}
+              alt=""
+              src={getImageURL(item.icon)}
+            /> */}
+            <SourceImage
+              className="icon"
+              data-isActive={String(currentModule.index === item.index)}
+              src={item.icon}
+            />
+          </StyleIcon>
+        </Tooltip>
       ))}
     </StyleModuleSelector>
   );
@@ -50,17 +55,24 @@ const StyleModuleSelector = styled.div`
   border-right: ${({ theme }) => theme["@border-color-base"]} 1px solid;
   padding: 80px 0;
 `;
-const StyleIcon = styled.div<{ isActive: boolean }>`
-  width: ${({ isActive }) => (isActive ? "55px" : "45px")};
-  opacity: ${({ isActive }) => (isActive ? 1 : 0.4)};
-  margin: 20px auto;
-  &:hover {
-    width: 60px;
-  }
-  transition: 0.4s all ease;
+const StyleIcon = styled.div`
+  width: 100%;
+  height: 65px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   .icon {
     cursor: pointer;
-    width: 100%;
+    width: 45px;
+    opacity: 0.4;
+    transition: 0.4s all ease;
+    &[data-isActive="true"] {
+      width: 55px;
+      opacity: 1;
+    }
+    /* &:hover {
+      width: 50px;
+    } */
   }
   .name {
     font-size: 12px;
